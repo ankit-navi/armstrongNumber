d3a7e791fd30070a30e587da6ee2149a739622dd
Ankit Kumar Gupta <ankitkumargupta@ITs-MacBook-Pro.local>
2020-01-16 10:34:20 +0530

    Add functionality to check armstrong numbers

---
+++ src/main/java/ArmstrongNumbers.java
@@ -0,0 +1,15 @@
+class ArmstrongNumbers {
+
+	boolean isArmstrongNumber(int numberToCheck) {
+		String num = Integer.toString(numberToCheck);
+		int n = num.length();
+		int sum=0;
+		for(int i=0; i<n; i++){
+			sum += Math.pow(num.charAt(i)-48, n);
+		}
+		if(sum == numberToCheck)
+			return true;
+		return false;
+	}
+
+}
